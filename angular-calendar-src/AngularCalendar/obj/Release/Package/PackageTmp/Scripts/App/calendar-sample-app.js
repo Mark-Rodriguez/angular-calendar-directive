227f903d11f8e5155385e87dffe6dc722149274b
Mark Rodriguez <mark.rodriguez.dev@gmail.com>
2015-10-04 20:36:37 -0500

    update

--- angular-calendar-src/AngularCalendar/obj/Release/Package/PackageTmp/Scripts/App/calendar-sample-app.js
+++ angular-calendar-src/AngularCalendar/obj/Release/Package/PackageTmp/Scripts/App/calendar-sample-app.js
@@ -5,12 +5,14 @@ angular.module("calendar-sample-app").controller("calendar-sample-master-control
     })
 
     $scope.settings = {
-        sampleURL: "SampleCalendars/basic-sample-calendar.html"
+        sampleURL: "SampleCalendars/basic-sample-calendar.html",
+        sampleExplainURL: "basic-sample-calendar-explain.html"
     };
 
     $scope.clickSample = function (sample)
     {
         $scope.settings.sampleURL = "SampleCalendars/" + sample;
+        $scope.settings.sampleExplainURL = sample.substring(0, sample.indexOf(".html")) + "-explain.html";
     }
 
 }]);
---
+++ sample-explain-controller.js
@@ -0,0 +1,10 @@
+﻿angular.module("calendar-sample-app").controller("sample-explain-controller", ["$scope", "$http", function ($scope, $http) {
+
+    $scope.sample = {
+        explain: "SampleCalendars/explain-basic-html.html"
+    };
+
+    $scope.setSample = function (sample) {
+        $scope.sample.explain = sample;
+    }
+}]);
